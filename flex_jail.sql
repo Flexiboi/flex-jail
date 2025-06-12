279480bb7836344d6ef90c7d72bdb2f54e55a876
Flex <45070800+Flexiboi@users.noreply.github.com>
2025-06-12 21:15:04 +0200

    added sql file

---
+++ flex_jail.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 12, 2025 at 09:16 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dinamixdev`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `flex_jail`
+--
+
+CREATE TABLE `flex_jail` (
+  `identifier` varchar(100) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0,
+  `cangooutside` tinyint(1) NOT NULL DEFAULT 0,
+  `jailtime` int(11) NOT NULL DEFAULT 0,
+  `prisonnumber` int(11) NOT NULL DEFAULT 0,
+  `items` longtext DEFAULT NULL,
+  `data` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `flex_jail`
+--
+ALTER TABLE `flex_jail`
+  ADD PRIMARY KEY (`identifier`) USING BTREE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
